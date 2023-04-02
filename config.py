f74dbb54cf8111c159c0fb2c339e3be6e716d5d4
saakethch <123130490+saakethch@users.noreply.github.com>
2023-04-02 19:07:26 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_keys = {
-    'OPENAI_API_KEY': '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',
+    'OPENAI_API_KEY': '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',
     'PINECONE_API_KEY': 'd18e6ea4-14f6-4dc1-83df-6397183f2582'
     }
