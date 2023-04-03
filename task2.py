1847bf99d1ef75bf64524526676adc0cd8fc8ea0
saakethch <chaganty.s@northeastern.edu>
2023-04-02 20:19:48 -0400

    changed

--- task2.py
+++ task2.py
@@ -1,21 +1,24 @@
 import os
 import streamlit as st
 import openai
-import config
-import time 
-openai.api_key = config.api_keys["OPENAI_API_KEY"]
+import time
+
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+
 def generate_image(image_type):
     response = openai.Image.create(
-    prompt= image_type,
-    n=1,
-    size="1024x1024"
+        prompt=image_type,
+        n=1,
+        size="1024x1024"
     )
     image1 = response['data'][0]['url']
     st.image(image=image1, caption="Your customized fashion T-Shirt")
 
+
 def task2():
-    tshirt_type = st.text_input("Enter the custom style for T-shirt:", "") + "printed on a"+ st.selectbox(f'Apparel type', options=['Blazer','Suit','Shirt', 'T-Shirt','Hoodie','Shorts','Jeans'])
-    image_type = tshirt_type
+    apparel_prompt = st.text_input("Enter the custom style for T-shirt:", "") + "printed on a" + st.selectbox(
+        f'Apparel type', options=['Blazer', 'Suit', 'Shirt', 'T-Shirt', 'Hoodie', 'Shorts', 'Jeans'])
     time.sleep(3)
-    if image_type!="":
-        generate_image(image_type)
+    if len(apparel_prompt)>10:
+        generate_image(apparel_prompt)
--- task3.py
+++ task3.py
@@ -5,7 +5,6 @@ from pathlib import Path
 import streamlit as st
 import pinecone
 import torch
-import config
 import torchvision
 from torchvision.transforms import (
     Compose, 
@@ -14,6 +13,8 @@ from torchvision.transforms import (
     ToTensor, 
     Normalize
 )
+from decouple import config
+
 
 def task3():
     st.title("Image Search 10k")
@@ -30,7 +31,7 @@ def task3():
         img = Image.open(uploaded_file)
         uploaded_img_path = "static/uploaded/" + datetime.now().isoformat().replace(":", "_") + uploaded_file.name
         img.save(uploaded_img_path)
-        pc_api_key = config.api_keys["PINECONE_API_KEY"]
+        pc_api_key = st.secrets["PINECONE_API_KEY"]
         pinecone.init(api_key=pc_api_key, environment="eu-west1-gcp")
         model = torchvision.models.squeezenet1_1(pretrained=True).eval()
         index = pinecone.Index("pinecone-image-search")
